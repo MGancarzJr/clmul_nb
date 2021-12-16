cfe39d43afcf7bbae60ca297d7519836612caa4a
MGancarzJr <mgancarzjr@gmail.com>
2021-12-16 09:06:22 -0600

    Added about info.

--- clmul_nb.h
+++ clmul_nb.h
@@ -1,12 +1,24 @@
-/* File clmul_nb.h */
+//////////////////////////////////////////////////////////////////////////////
+// File: clmul_nb.h
+// Author: Maciej Gancarz Jr
+// Date: December 16, 2021 
+// Version: 1.0
+// License: GNU General Public License v3.0
+//
+// clmul_nb accepts two unsigned integers and performs a carry-less multi-
+// plication operation on the lowest bytes. This operation is defined in NIST
+// FIPS 197 Advanced Encryption Standard (AES) § 4.3 which is available at the
+// url: https://nvlpubs.nist.gov/nistpubs/fips/nist.fips.197.pdf
+//
+// The purpose of this library is to provide a low-level algorithm for chip
+// architectures which do not support cryptographic acceleration. The algo-
+// rithm is designed to avoid branching conditions in the expansion and re-
+// duction phases of the polynomial multiplication.
+//////////////////////////////////////////////////////////////////////////////
+
 #ifndef CLMUL_NB_H_
 #define CLMUL_NB_H_
 
-/* Carry-less multiplication of two bytles.
-*
-*
-* clmul_nb takes two unsigned integers and returns an unsigned integer. Only the lowest 8 bits of each integers will be considered. This operation is defined in NIST FIPS 197 §4.2 (https://nvlpubs.nist.gov/nistpubs/fips/nist.fips.197.pdf).
-*/
 unsigned int clmul_nb(unsigned int first_polynomial, unsigned int second_polynomial);
 
 #endif
